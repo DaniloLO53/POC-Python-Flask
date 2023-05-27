3ae6e854d90ff423729aed7d7f250cb657c36228
Danilo Leão <olin_ad@hotmail.com>
2023-05-27 14:55:46 -0300

    feat: get students and create students

--- main.py
+++ main.py
@@ -1,8 +1,100 @@
-from flask import Flask
+import sqlite3
+import json
+from flask import Flask, g, request
+from markupsafe import escape
+
+DATABASE_PATH = "students.db"
+
+
+def get_db():
+    db = getattr(g, "_database", None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE_PATH)
+    db.row_factory = sqlite3.Row
+    return db
+
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
 
 app = Flask(__name__.split('.')[0])
 
 
-@app.route("students")
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+def init_db():
+    studentsSchemaPath = "./schemas/students.sql"
+
+    with app.app_context():
+        db = get_db()
+        with app.open_resource(studentsSchemaPath, mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+init_db()
+
+
+@app.post("/students")
+def create():
+    studentData = json.loads(request.data)
+    query_db(
+        'INSERT INTO students(matricula, nome, sobrenome, email, telefone, curso, nascimento, updated_at)'
+        'VALUES(?, ?, ?, ?, ?, ?, ?, ?)',
+        [
+            studentData["matricula"],
+            studentData["nome"],
+            studentData["sobrenome"],
+            studentData["email"],
+            studentData["telefone"],
+            studentData["curso"],
+            studentData["nascimento"],
+            studentData["updated_at"]
+        ]
+    )
+    students = getStudent(studentData["matricula"])
+    return students
+
+
+@app.get("/students")
 def getAll():
-    return "hello world"
+    DbStudents = query_db('SELECT * FROM students')
+    students = []
+
+    for student in DbStudents:
+        students.append({
+            "Matrícula": student['matricula'],
+            "Nome": student['nome'],
+            "Sobrenome": student['sobrenome'],
+            "Email": student['email'],
+            "Telefone": student['telefone'],
+            "Curso": student['curso'],
+            "Nascimento": student['nascimento'],
+        })
+    return students
+
+
+def getStudent(matricula):
+    print("Matricula:", matricula)
+
+    query = (
+        "SELECT matricula, nome, sobrenome, email, telefone, curso, nascimento "
+        "FROM students "
+        f"WHERE students.matricula = {matricula}"
+    )
+
+    student = query_db(query)
+    student_dict = dict(student[0])
+    return json.dumps(student_dict, default=str)
+
+# Diferença entre json.load e json.loads (erro )
+# https://stackoverflow.com/questions/6541767/python-urllib-error-attributeerror-bytes-object-has-no-attribute-read
---
+++ schemas/students.sql
@@ -0,0 +1,91 @@
+CREATE TABLE
+IF NOT EXISTS students
+(
+  matricula INTEGER PRIMARY KEY,
+  nome VARCHAR
+(255) NOT NULL,
+  sobrenome VARCHAR
+(255) NOT NULL,
+  email VARCHAR
+(255) NOT NULL,
+  telefone VARCHAR
+(20) NOT NULL,
+  curso VARCHAR
+(255) NOT NULL,
+  nascimento DATE NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT
+CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL
+);
+
+INSERT INTO students
+  (matricula, nome, sobrenome, email, telefone, curso, nascimento, updated_at)
+SELECT 1001, 'João', 'Silva', 'joao.silva@example.com', '2165456545', 'Engenharia Mecânica', '1995-07-15', CURRENT_TIMESTAMP
+WHERE NOT EXISTS (SELECT 1
+FROM students
+WHERE matricula = 1001)
+LIMIT 1;
+
+INSERT INTO students
+  (matricula, nome, sobrenome, email, telefone, curso, nascimento, updated_at)
+SELECT 1002, 'Maria', 'Santos', 'maria.santos@example.com', '2195765456', 'Administração', '1994-09-20', CURRENT_TIMESTAMP
+WHERE NOT EXISTS (SELECT 1
+FROM students
+WHERE matricula = 1002)
+LIMIT 1;
+
+INSERT INTO students
+  (matricula, nome, sobrenome, email, telefone, curso, nascimento, updated_at)
+SELECT 1003, 'Pedro', 'Ferreira Alberto', 'pedro.ferreira@example.com', '2233654568', 'Medicina', '1996-02-10', CURRENT_TIMESTAMP
+WHERE NOT EXISTS (SELECT 1
+FROM students
+WHERE matricula = 1003)
+LIMIT 1;
+
+INSERT INTO students
+  (matricula, nome, sobrenome, email, telefone, curso, nascimento, updated_at)
+SELECT 1004, 'Rafael', 'Alves da Cunha', 'rafael.alves@example.com', '21634789745', 'Medicina', '1996-02-10', CURRENT_TIMESTAMP
+WHERE NOT EXISTS (SELECT 1
+FROM students
+WHERE matricula = 1004)
+LIMIT 1;
+
+INSERT INTO students
+  (matricula, nome, sobrenome, email, telefone, curso, nascimento, updated_at)
+SELECT 1005, 'Carlos', 'da Silva Pinto', 'carlos.silva@example.com', '21995487897', 'Artes Visuais', '1992-06-10', CURRENT_TIMESTAMP
+WHERE NOT EXISTS (SELECT 1
+FROM students
+WHERE matricula = 1005)
+LIMIT 1;
+
+INSERT INTO students
+  (matricula, nome, sobrenome, email, telefone, curso, nascimento, updated_at)
+SELECT 1006, 'Andressa', 'Moraes da Cunha', 'andressa.moraes@example.com', '1145678479', 'Engenharia Mecânica', '1991-02-12', CURRENT_TIMESTAMP
+WHERE NOT EXISTS (SELECT 1
+FROM students
+WHERE matricula = 1006)
+LIMIT 1;
+
+INSERT INTO students
+  (matricula, nome, sobrenome, email, telefone, curso, nascimento, updated_at)
+SELECT 1007, 'José', 'Pereira de Oliveira', 'jose.pereira@example.com', '2145612345', 'Engenharia Naval', '1995-11-10', CURRENT_TIMESTAMP
+WHERE NOT EXISTS (SELECT 1
+FROM students
+WHERE matricula = 1007)
+LIMIT 1;
+
+INSERT INTO students
+  (matricula, nome, sobrenome, email, telefone, curso, nascimento, updated_at)
+SELECT 1008, 'Bernardo', 'Silva Souto', 'bernardo.silva@example.com', '11997845678', 'Música', '1996-03-10', CURRENT_TIMESTAMP
+WHERE NOT EXISTS (SELECT 1
+FROM students
+WHERE matricula = 1008)
+LIMIT 1;
+
+INSERT INTO students
+  (matricula, nome, sobrenome, email, telefone, curso, nascimento, updated_at)
+SELECT 1009, 'Letícia', 'Drummound Lopes', 'leticia.lopes@example.com', '2154564563', 'Música', '1997-05-05', CURRENT_TIMESTAMP
+WHERE NOT EXISTS (SELECT 1
+FROM students
+WHERE matricula = 1008)
+LIMIT 1;
